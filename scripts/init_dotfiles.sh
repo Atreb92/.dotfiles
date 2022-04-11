e30e58fe903237f82fa7e8826faf64ff4b9e6ae1
kali <atreb92@gmail.com>
2022-04-11 15:40:41 +0200

    Init dotfiles

---
+++ scripts/init_dotfiles.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# i3
+mkdir -p ~/.config/i3
+ln -sf ~/.dotfiles/i3 ~/.config/i3/config
+
+# i3status
+mkdir -p ~/.config/i3status
+ln -sf ~/.dotfiles/i3status ~/.config/i3status/config
+
+# flameshot
+mkdir -p ~/.config/flameshot
+ln -sf ~/.dotfiles/flameshot ~/.config/flameshot/flameshot.ini
+
+# flameshot
+mkdir -p ~/.config/terminator
+ln -sf ~/.dotfiles/terminator ~/.config/terminator/config
+
+# vimrc
+ln -sf ~/.dotfiles/vimrc ~/.vimrc
+
+# zsh + powerlevel10k
+ln -sf ~/.dotfiles/zshrc ~/.zshrc
+ln -sf ~/.dotfiles/p10k.zsh ~/.p10k.zsh
+
+# bash
+ln -sf ~/.dotfiles/bashrc ~/.bashrc
